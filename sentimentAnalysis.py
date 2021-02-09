5de827ad81fd21614eedab7f9b77dcfcb574bcf6
Davydzz <davydzz@users.noreply.github.com>
2021-02-09 16:22:44 +0100

    commit

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -3,10 +3,52 @@ import sqlite3
 
 from nltk.sentiment import SentimentIntensityAnalyzer
 obj = SentimentIntensityAnalyzer()
+
+
+
+def create_connection(databaseFile):
+    """ 
+    :param databaseFile: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(databaseFile)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+# Input is sessionID, will probably be the pk
+
+def getFromDB(sessionID, conn):
+
+    connection = conn.cursor()
+    # Skeleton for future database
+    connection.execute("SELECT * FROM tableName WHERE sessionID = ? ", sessionID) # will probably need to add time to it
+    
+    return connection.fetchAll()
+
+
+def getSentimentScore(msgs, obj):
+
+    sentimentList = []
+    for row in msgs:
+        score = obj.polarity_scores(row)
+        sentimentList.append(score['compound'])
+
+    return sentimentList
+
+def averageSentiment(list):
+    return sum(list) / len(list)
+
+
+
 #obj.polarity_scores("Wow, NLTK is really powerful!")
 #print(obj.polarity_scores("Wow, NLTK is really powerful!"))
 #output= {'neg': 0.0, 'neu': 0.295, 'pos': 0.705, 'compound': 0.8012}
-
+#print(type(output))
 #print(obj.polarity_scores("I'm sad"))
 #{'neg': 0.756, 'neu': 0.244, 'pos': 0.0, 'compound': -0.4767}
 
@@ -19,3 +61,6 @@ obj = SentimentIntensityAnalyzer()
 #print(obj.polarity_scores("I ran over your cat lol"))
 #{'neg': 0.0, 'neu': 0.588, 'pos': 0.412, 'compound': 0.4215} ?????
 
+conn = create_connection("database.db")
+msgs = getFromDB(2, conn)
+listSentiments = getSentimentScore(msgs, obj)
