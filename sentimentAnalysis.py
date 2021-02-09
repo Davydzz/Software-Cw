a510f7f237df557bb19d097890b155879c9eb217
Davydzz <davydzz@users.noreply.github.com>
2021-02-09 15:42:27 +0100

    tests

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -1,4 +1,5 @@
 import nltk
+import sqlite3
 
 from nltk.sentiment import SentimentIntensityAnalyzer
 obj = SentimentIntensityAnalyzer()
@@ -12,5 +13,9 @@ obj = SentimentIntensityAnalyzer()
 #print(obj.polarity_scores("This is literally the worst presentation I've ever witnessed. Give me back my time."))
 #{'neg': 0.24, 'neu': 0.76, 'pos': 0.0, 'compound': -0.6249}
 
-print(obj.polarity_scores("I enjoyed the presentation but the sound was a bit quiet"))
+#print(obj.polarity_scores("I enjoyed the presentation but the sound was a bit quiet"))
 #{'neg': 0.0, 'neu': 0.788, 'pos': 0.212, 'compound': 0.2846}
+
+#print(obj.polarity_scores("I ran over your cat lol"))
+#{'neg': 0.0, 'neu': 0.588, 'pos': 0.412, 'compound': 0.4215} ?????
+
