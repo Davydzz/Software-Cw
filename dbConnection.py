92562ca5cea6abf060f8dafd2edebde506581cee
Rebecca French <57105778+rebeccajfrench@users.noreply.github.com>
2021-03-09 14:41:34 +0000

    Not vulnerable to SQL injections

--- dbConnection.py
+++ dbConnection.py
@@ -21,14 +21,14 @@ class DBConnection:
     def addUser(self, salt, password, fName, lName, email): #used for registration
         #check user is not already in db
         conn = self.createConnection(self.database)
-        existingUserStatement = ("SELECT * FROM users WHERE email = '%s'" % email)
-        for row in conn.execute(existingUserStatement):
+        existingUserStatement = "SELECT * FROM users WHERE email = ?;" #? = email
+        for row in conn.execute(existingUserStatement,(email,)):
             #print(row)
             #already exists
             return False, None
 
         if conn is not None:
-            insertStatement = ("INSERT INTO users (salt, password, firstName, lastName, email) VALUES (?, ?, ?, ?, ?)")
+            insertStatement = ("INSERT INTO users (salt, password, firstName, lastName, email) VALUES (?, ?, ?, ?, ?);")
             conn.execute(insertStatement,(salt, password, fName, lName, email))
             conn.commit()
             userID = self.getIDFromEmail(email)
@@ -41,10 +41,11 @@ class DBConnection:
 
         if conn is not None:
             if userID == None:
-                insertStatement = ("INSERT INTO feedback (anonymous, timestamp, feedbackFormID, roomcode, sentiment) VALUES ('%s', '%s', '%s', '%s', '%s')" % (anonymous, timestamp, feedbackFormID, roomcode, sentiment))
+                insertStatement = "INSERT INTO feedback (anonymous, timestamp, feedbackFormID, roomcode, sentiment) VALUES (?, ?, ?, ?, ?);" # (anonymous, timestamp, feedbackFormID, roomcode, sentiment)
+                conn.execute(insertStatement,(anonymous, timestamp, feedbackFormID, roomcode, sentiment))
             else:
-                insertStatement = ("INSERT INTO feedback (userID, anonymous, timestamp, feedbackFormID, roomcode, sentiment) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (userID, anonymous, timestamp, feedbackFormID, roomcode, sentiment))
-            conn.execute(insertStatement)
+                insertStatement = "INSERT INTO feedback (userID, anonymous, timestamp, feedbackFormID, roomcode, sentiment) VALUES (?, ?, ?, ?, ?, ?);" # (userID, anonymous, timestamp, feedbackFormID, roomcode, sentiment)
+                conn.execute(insertStatement,(userID, anonymous, timestamp, feedbackFormID, roomcode, sentiment))
             id = conn.execute('select last_insert_rowID();')
             id = id.fetchone()
             id = id[0]
@@ -56,9 +57,9 @@ class DBConnection:
         if conn is not None:
 
             cur = conn.cursor()
-            checkIfAddedBefore= ("SELECT feedbackID FROM feedback WHERE userID = ? AND roomcode  = ?")
-            insertStatement =  ("INSERT INTO feedbackQuestions (questionID, feedbackID, answer) VALUES (?, ?, ?)" )
-            updateAnswer = ("UPDATE feedbackQuestions SET answer = ? WHERE feedbackID = ? ")
+            checkIfAddedBefore= ("SELECT feedbackID FROM feedback WHERE userID = ? AND roomcode  = ?;")
+            insertStatement =  ("INSERT INTO feedbackQuestions (questionID, feedbackID, answer) VALUES (?, ?, ?);" )
+            updateAnswer = ("UPDATE feedbackQuestions SET answer = ? WHERE feedbackID = ?;")
             cur.execute(checkIfAddedBefore, (user,room))
             possibleBefore = cur.fetchall()
             print(room)
@@ -77,8 +78,8 @@ class DBConnection:
         conn = self.createConnection(self.database)
 
         if conn is not None:
-            insertStatement = ("INSERT INTO feedbackform (eventID, overallSentiment) VALUES ('%s', '%s')" % (eventID, overallSentiment))
-            conn.execute(insertStatement)
+            insertStatement = "INSERT INTO feedbackform (eventID, overallSentiment) VALUES (?, ?);" #?1 = eventID, ?2 = overallSentiment
+            conn.execute(insertStatement,(eventID, overallSentiment))
             id = conn.execute('select last_insert_rowID();')
             id = id.fetchone()
             id = id[0]
@@ -91,46 +92,46 @@ class DBConnection:
         
 
         if conn is not None:
-            hostStatement = ("SELECT roomcode, eventName FROM events WHERE hostUserID = '%s';" % userID)
-            attendeeStatement = ("SELECT events.roomcode, events.eventName FROM events INNER JOIN event_members ON events.roomcode = event_members.roomcode WHERE event_members.userID = '%s' AND events.active = 1;" % userID)
+            hostStatement = "SELECT roomcode, eventName FROM events WHERE hostUserID = ?;" #? = userID
+            attendeeStatement = "SELECT events.roomcode, events.eventName FROM events INNER JOIN event_members ON events.roomcode = event_members.roomcode WHERE event_members.userID = ? AND events.active = 1;" #? = userID
             
             hostRows = []
             attendeeRows = []
-            for row in conn.execute(hostStatement):
+            for row in conn.execute(hostStatement,(userID,)):
                 hostRows.append([row[0], row[1]])
 
-            for row in conn.execute(attendeeStatement):
+            for row in conn.execute(attendeeStatement,(userID,)):
                 attendeeRows.append([row[0], row[1]])
             return hostRows, attendeeRows
 
 
     def confirmLogin(self, email, password):
         conn = self.createConnection(self.database)
-        getSaltStatement = ("SELECT salt FROM users WHERE email = '%s';" % email)
+        getSaltStatement = "SELECT salt FROM users WHERE email = ?;"
         saltFound = False
-        for row in conn.execute(getSaltStatement):
+        for row in conn.execute(getSaltStatement,(email,)):
             saltFound = True
             salt = row[0]
         if not saltFound:
             return False, None
         toHash = salt + password
         hashedPassword = hashlib.sha256(bytes(toHash,"utf-8")).hexdigest()
-        existingUserStatement = ("SELECT * FROM users WHERE email = '%s' AND password = '%s'" % (email, hashedPassword))
-        for row in conn.execute(existingUserStatement):
+        existingUserStatement = "SELECT * FROM users WHERE email = ? AND password = ?;"
+        for row in conn.execute(existingUserStatement,(email, hashedPassword)):
             #password matches the email
             return True, row[0]
         return False, None
 
     def getIDFromEmail(self,email):
         conn = self.createConnection(self.database)
-        getUserStatement = ("SELECT userID FROM users WHERE email = '%s';" % email)
-        for row in conn.execute(getUserStatement):
+        getUserStatement = "SELECT userID FROM users WHERE email = ?;"
+        for row in conn.execute(getUserStatement,(email,)):
             return row[0]
 
     def getUserFromUserID(self, uID):
         conn = self.createConnection(self.database)
-        getUserStatement = ("SELECT * FROM users WHERE userID = '%s';" % uID)
-        for row in conn.execute(getUserStatement):
+        getUserStatement = "SELECT * FROM users WHERE userID = ?;"
+        for row in conn.execute(getUserStatement,(uID,)):
             return row
 
 
@@ -151,8 +152,8 @@ class DBConnection:
             roomcode = str(roomcode)
 
             conn = self.createConnection(self.database)
-            existingEventStatement = ("SELECT * FROM events WHERE roomcode = '%s'" % roomcode)
-            for row in conn.execute(existingEventStatement):
+            existingEventStatement = "SELECT * FROM events WHERE roomcode = ?;" #? = roomcode
+            for row in conn.execute(existingEventStatement,(roomcode,)):
                 validCode = False
         if conn is not None:
             insertStatement = ("INSERT INTO events (roomcode, eventName, feedbackFrequency, hostUserID, date, active, feedbackFormID) VALUES (?, ?, ?, ?, ?, ?, ?)")
@@ -165,17 +166,17 @@ class DBConnection:
         conn = self.createConnection(self.database)
 
         #if room code exists in events
-        eventExistsStatement = ("SELECT * FROM events WHERE roomCode = '%s';" % roomCode)
+        eventExistsStatement = "SELECT * FROM events WHERE roomCode = ?;" #? = roomCode
         roomCodeExists = False
-        for row in conn.execute(eventExistsStatement):
+        for row in conn.execute(eventExistsStatement,(roomCode,)):
             roomCodeExists = True
 
         if roomCodeExists:
             if userID != None:
                 try:
                     #then add userID to event_members
-                    addUserToEventsStatement = ("INSERT INTO event_members (roomcode, userID) VALUES ('%s', '%s')" % (roomCode, userID))
-                    conn.execute(addUserToEventsStatement)
+                    addUserToEventsStatement = "INSERT INTO event_members (roomcode, userID) VALUES (?, ?);" # ?1 = roomCode, ?2 = userID)
+                    conn.execute(addUserToEventsStatement,(roomCode, userID))
                     conn.commit()
                 except:
                     print("You are already in the room")
@@ -184,7 +185,6 @@ class DBConnection:
         print("Room code doesn't exist")
         return False
 
-    # Fix bug tmr morning
     def addTemplate(self, result, templateName):
 
         conn = self.createConnection(self.database)
@@ -237,10 +237,10 @@ class DBConnection:
         #get all questions where feedback ID is NULL and feedback form ID matches
         #return these questions
         conn = self.createConnection(self.database)
-        getQuestionsStatement = ("SELECT * FROM Question INNER JOIN events ON Question.feedbackFormID = events.feedbackFormID WHERE events.roomcode = '%s';" % eventID)
+        getQuestionsStatement = "SELECT * FROM Question INNER JOIN events ON Question.feedbackFormID = events.feedbackFormID WHERE events.roomcode = ?;" #? =  eventID
         feedbackQuestions = []
         questionIDs = []
-        for row in conn.execute(getQuestionsStatement):
+        for row in conn.execute(getQuestionsStatement,(eventID,)):
             questionIDs.append(row[0])
             questionNumber = row[1]
             questionType = row[2]
@@ -251,11 +251,10 @@ class DBConnection:
         return feedbackQuestions, feedbackFormID, questionIDs
 
     def getFeedbackTemplate(self, templateName):
-
         conn = self.createConnection(self.database)
-        getQuestionsStatement = ("SELECT * FROM Question WHERE feedbackFormID = (SELECT feedbackFormID from feedbackform WHERE templateName = '%s');" % templateName)
+        getQuestionsStatement = "SELECT * FROM Question WHERE feedbackFormID = (SELECT feedbackFormID from feedbackform WHERE templateName = ?);" #? = templateName
         feedbackQuestions = []
-        for row in conn.execute(getQuestionsStatement):
+        for row in conn.execute(getQuestionsStatement,(templateName,)):
             questionNumber = row[1]
             questionType = row[2]
             questionName = row[3]
@@ -267,13 +266,13 @@ class DBConnection:
     def getAnswers(self, roomCode):
 
         conn = self.createConnection(self.database)
-        getQuestionID = ("SELECT questionID from Question WHERE feedbackFormID = (SELECT feedbackFormID FROM events WHERE roomcode= '%s');" %roomCode)
+        getQuestionID = "SELECT questionID from Question WHERE feedbackFormID = (SELECT feedbackFormID FROM events WHERE roomcode= ?);" #? =roomCode
         getQuestions = ("SELECT content,type from Question WHERE questionID = ?")
         getAnswers=  ("SELECT answer from feedbackQuestions INNER JOIN feedback ON feedbackQuestions.feedbackID = feedback.feedbackID WHERE feedbackQuestions.questionID = ? AND feedback.roomcode = ?")
         questionAns = []
         nonCompounded= []
 
-        for elem in conn.execute(getQuestionID):
+        for elem in conn.execute(getQuestionID,(roomCode,)):
             questionID = elem[0]
 
             for qs in conn.execute(getQuestions, (questionID,)):
